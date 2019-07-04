aa3184a6c05981e76b11ec981ac91b7a24cac0dc
adder <a.j.orozco038@hotmail.com>
2019-07-04 09:47:45 -0600

    urls y modelo en app inventario

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
 
-# Create your models here.
+class Producto(models.Model):
+    codigo_de_barras = models.CharField(max_length=12, blank=False, null=False, unique=True)
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField(default='', blank=True, null=True)
+    precio_venta_uno = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=0.0)
+    precio_venta_dos = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=0.0)
+    precio_costo = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=0.0)
+    observaciones = models.TextField(default='', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
